7e8662f531608a32d5582ee75bd7a2434c418f02
GRimkiller360 <ethanmglindeque@gmail.com>
2026-07-20 08:03:11 +0200

    Revert "Merge pull request #1 from GRimkiller360/cloudflare/workers-autoconfig"
    This reverts commit 38b6d5bd5c4b89b30626645e42ed6656d3e889e5, reversing
    changes made to 365e6a893a6982782a257285db5c4eea4fbf5ee2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   // Update this to your custom domain once you connect one in the
@@ -12,6 +10,5 @@ export default defineConfig({
   // no base path needed unless you set a custom path in the dashboard.
   site: 'https://bracketly.pages.dev',
 
-  integrations: [sitemap()],
-  adapter: cloudflare()
+  integrations: [sitemap()]
 });
